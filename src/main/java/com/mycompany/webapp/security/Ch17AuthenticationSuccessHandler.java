ccf9294c1143f340609fdbdf0daf8c204ed12a41
kayjuni <pjk146195@gmail.com>
2021-09-17 14:56:03 +0900

    fix

--- src/main/java/com/mycompany/webapp/security/Ch17AuthenticationSuccessHandler.java
+++ src/main/java/com/mycompany/webapp/security/Ch17AuthenticationSuccessHandler.java
@@ -3,7 +3,6 @@ package com.mycompany.webapp.security;
 
 import java.io.IOException;
 
-import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,20 +10,27 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+/*
+SimpleUrlAuthenticationSuccessHandler
+   로그인 성공후에 무조건 홈으로 이동
+SavedRequestAwareAuthenticationSuccessHandler
+   로그인 성공후에 사용자가 접근하고자 했던 페이지로 이동
+*/
+
 @Component
-public class Ch17AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
+//public class Ch17AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
+public class Ch17AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler{	
 	private static final Logger logger = LoggerFactory.getLogger(Ch17AuthenticationSuccessHandler.class);
 	
 	@Override
 	public void onAuthenticationSuccess(
 			HttpServletRequest request, 
 			HttpServletResponse response, 
-			FilterChain chain,
 			Authentication authentication) throws IOException, ServletException {
 		logger.info("실행");
-		super.onAuthenticationSuccess(request, response, chain, authentication);
+		super.onAuthenticationSuccess(request, response, authentication);
 	}
 }
